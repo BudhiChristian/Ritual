913e130a4b7ac60c12c7d530e649ffdd017b02ed
BudhiChristian <cjbudhi95@gmail.com>
2022-10-18 22:17:41 -0400

    Different indicator for how many spirits captured

--- scripts/state_management/soul_jar_state.gd
+++ scripts/state_management/soul_jar_state.gd
@@ -1,6 +1,7 @@
 extends Node
 
 var held_souls: Array = []
+var jar_color: Color = Color.GRAY
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,15 +10,18 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	get_node("spirit_count").text = str(held_souls.size())
+	get_node("spirit_indicator_1").visible = held_souls.size() > 0
+	get_node("spirit_indicator_2").visible = held_souls.size() > 1
+	get_parent().modulate = Color.GRAY if held_souls.is_empty() else jar_color
 	
 func _mark_captured(spirit: Node) -> void:
-	if held_souls.size() == 0 || held_souls[0].spirit_color == spirit.spirit_color:
+	if held_souls.is_empty() or jar_color == spirit.spirit_color:
+		jar_color = spirit.spirit_color
 		held_souls.append(spirit)
 	# we could add some process instead of automatically doing this
-	if held_souls.size() == 3:
+	if held_souls.size() >= 3:
 		MessageBus.exorcise_spirits_in_jar.emit(held_souls)
 		held_souls = []
 
 func _spirit_escape(spirit: Node) -> void:
-	held_souls = held_souls.filter(func(held): return held != spirit)	
+	held_souls = held_souls.filter(func(held): return held != spirit)
