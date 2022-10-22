5d4a848ae5f035ebcd497f6b972b7f777e576295
BudhiChristian <cjbudhi95@gmail.com>
2022-10-22 12:47:15 -0400

    beginning script for two spirits

--- ritual_dialogues/02_two_spirits/02_two_spirits.gd
+++ ritual_dialogues/02_two_spirits/02_two_spirits.gd
@@ -4,14 +4,19 @@ extends RitualDialogueHandler
 var spirit_color_1 = Color8(172, 91, 53)
 var spirit_color_2 = Color8(55, 120, 81)
 
+# dialog
+var _00_intro = preload("res://ritual_dialogues/02_two_spirits/02_00_intro.dialogue")
+
 # TODO dialogue
 func _ready() -> void:
 	super()
 	await get_tree().current_scene.ready
 	MessageBus.host_is_possessed.connect(possessed)
+
 	first_set_of_spirits()
 	
 func first_set_of_spirits():
+	await progress_dialog(_00_intro, "start")
 	MessageBus.spawn_spirit_trio.emit(spirit_color_1)
 	MessageBus.spawn_spirit_trio.emit(spirit_color_1)
 	
