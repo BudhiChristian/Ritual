7b1cf947e3adfe28f7d45e6535a4f4c973d04900
Nicholas Maddalena <NicholasJmaddalena@gmail.com>
2022-10-22 22:20:57 -0400

    thurible is now click-and-drag

--- scripts/state_management/thurible_state.gd
+++ scripts/state_management/thurible_state.gd
@@ -2,6 +2,9 @@ extends BaseState
 
 @export var smoke_prefab:PackedScene
 
+var last_mouse_position = null
+var is_mouse_held = false
+
 func _ready() -> void:
 	super()
 	self.tool_name = "thurible"
@@ -9,10 +12,25 @@ func _ready() -> void:
 func handle_input(event:InputEvent):
 	if event is InputEventMouseButton:
 		if event.is_pressed() and event.button_index == 1:
-			var new_smoke = smoke_prefab.instantiate()
-			new_smoke.position = event.position
-			get_tree().current_scene.add_child(new_smoke)
+			make_smoke_at(event.position)
+			is_mouse_held = true
+		if !event.is_pressed() and event.button_index == 1:
+			last_mouse_position = null
+			is_mouse_held = false
+	
+	if event is InputEventMouseMotion and is_mouse_held:
+		if last_mouse_position.distance_to(event.position) > 80:
+			make_smoke_at(event.position)
 
+func make_smoke_at(p):
+	var new_smoke = smoke_prefab.instantiate()
+	new_smoke.position = p
+	last_mouse_position = p
+	get_tree().current_scene.add_child(new_smoke)
+
+func on_exit():
+	is_mouse_held = false
+	last_mouse_position = null
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
